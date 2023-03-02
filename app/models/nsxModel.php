cad15006b4764ce829d44ca5c941081e4016b1c3
DanKT15 <105506442+DanKT15@users.noreply.github.com>
2023-03-02 17:08:16 +0700

    2/3/2023

---
+++ app/models/nsxModel.php
@@ -0,0 +1,36 @@
+<?php
+
+class nsxModel extends Model
+{
+    private $_table = 'nhasanxuat';
+
+    public function POST($data=[]) // Tạo dữ liệu mới
+    {
+        $query = $this->db->insert($this->_table, $data)->fetchAll(PDO::FETCH_ASSOC);
+        return $query;
+    }
+
+    public function GETALL() // Lấy dữ liệu về
+    {
+        $query = $this->db->query("SELECT * FROM $this->_table")->fetchAll(PDO::FETCH_ASSOC);
+        return $query;
+    }
+
+    public function GETID($condition = '') // Lấy dữ liệu về
+    {
+        $query = $this->db->query("SELECT * FROM $this->_table WHERE $condition")->fetchAll(PDO::FETCH_ASSOC);
+        return $query;
+    }
+
+    public function PUT($data=[], $condition = '') // Cập nhật dữ liệu
+    {
+        $query = $this->db->update($this->_table, $data, $condition = '')->fetchAll(PDO::FETCH_ASSOC);
+        return $query;
+    }
+
+    public function DELETE($condition = '') // Xóa dữ liệu
+    {
+        $query = $this->db->delete($this->_table, $condition = '')->fetchAll(PDO::FETCH_ASSOC);
+        return $query;
+    }
+}
--- configs/database.php
+++ configs/database.php
@@ -3,7 +3,7 @@
 $config['database'] = [
     'host' => "localhost",
     'user' => "root",
-    'db' => "ban hang"
+    'db' => "shopdb"
 ];
 
 
