cc203f5ceeee38c91fda84208238ada37912cbc8
DanKT15 <105506442+DanKT15@users.noreply.github.com>
2023-03-14 12:39:07 +0700

    14/3/2023

---
+++ app/views/admin/taikhoan/add-tk.php
@@ -0,0 +1,59 @@
+<main class="app-content">
+    <div class="app-title">
+      <ul class="app-breadcrumb breadcrumb">
+        <li class="breadcrumb-item">Danh sách Tài Khoản</li>
+        <li class="breadcrumb-item">Thêm Tài Khoản</li>
+      </ul>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <div class="tile">
+          <h3 class="tile-title">Tạo mới tài khoản</h3>
+          <div class="tile-body">
+            
+            <form class="row">
+
+              <div class="form-group col-md-3">
+                <label class="control-label">Họ và Tên</label>
+                <input class="form-control" type="text">
+              </div>
+
+              <div class="form-group col-md-3">
+                <label class="control-label">Địa Chỉ</label>
+                <input class="form-control" type="text">
+              </div>
+
+              <div class="form-group col-md-3">
+                <label class="control-label">SDT</label>
+                <input class="form-control" type="text">
+              </div>
+
+              <div class="form-group col-md-3">
+                <label class="control-label">Email</label>
+                <input class="form-control" type="text">
+              </div>
+
+              <div class="form-group col-md-3">
+                <label class="control-label">Pass</label>
+                <input class="form-control" type="text">
+              </div>
+
+              <div class="form-group col-md-3 ">
+                <label for="exampleSelect1" class="control-label">Quyền</label>
+                <select class="form-control" id="exampleSelect1">
+                  <option>0</option>
+                  <option>1</option>
+                </select>
+              </div>
+
+              </div>
+                <button class="btn btn-save" type="button">Lưu lại</button>
+                <a class="btn btn-cancel" href="table-data-product.html">Hủy bỏ</a>
+              </div>
+
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+</main>
